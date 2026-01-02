9b23c2dd5fb4e664b309bcbcbe4775966c755b43
Yasir Raees Khan <khanyasirraees70@gmail.com>
2026-01-02 16:44:02 +0500

    CORS support added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // if you need cookies/auth headers
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
